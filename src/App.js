a8ab74c6f782c5083e9135df15fadea203f3be63
hunnurjirao <hunnurjirao2000.com>
2021-07-20 23:33:24 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
+import { useEffect } from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Learning from './components/Learning';
 import News from './components/News';
 import WebScrapping from './Learning/WebScrapping';
-
+import ReactGA from 'react-ga';
 
 
 function App() {
+
+  useEffect(() => {
+
+    ReactGA.initialize('UA-202704662-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  });
+
   return (
 
     <div className='container'>
@@ -28,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-202704662-1');
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
